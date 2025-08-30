b42136170024b505145b8d3c4bfc1d51bbb2dda3
gualin <iiwwpr1@gmail.com>
2025-08-30 13:50:57 +0800

    新增環境變數載入功能以動態配置資料庫連接字串

--- internal/config/database.go
+++ internal/config/database.go
@@ -1,14 +1,29 @@
 package config
 
 import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func InitDB() (*gorm.DB, error) {
-	// 資料庫連接字串
-	dsn := "root:@tcp(127.0.0.1:3307)/English_word_cards?charset=utf8mb4&parseTime=True&loc=Local"
+    err := godotenv.Load()
+	if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	db_name := os.Getenv("DB_NAME")
 	
+	// 資料庫連接字串
+	dsn := user + ":" + password + "@tcp(" + host + ":" + port + ")/"+ db_name+ "?charset=utf8mb4&parseTime=True&loc=Local"
+
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
